a6edd51c3cee1dff3d993e188fc48d00b8493c4e
Demy437 <97069175@st.deltion.nl>
2021-11-22 14:13:08 +0100

    Image upload systeem

--- Fotowedstrijd-laravel/database/migrations/2021_11_22_123340_create_images_table.php
+++ Fotowedstrijd-laravel/database/migrations/2021_11_22_123340_create_images_table.php
@@ -1,10 +1,10 @@
 <?php
-
+  
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
-class CreatePersonalAccessTokensTable extends Migration
+  
+class CreateImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,15 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64);
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->text('detail');
+            $table->string('image');
             $table->timestamps();
         });
     }
-
+  
     /**
      * Reverse the migrations.
      *
@@ -31,6 +29,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('images');
     }
 }
